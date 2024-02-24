e14656469e19a19b59e7df185af364bc7ab66184
your name goes here <jarolimfilip07@gmail.com>
2024-02-24 11:25:15 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+import localFont from 'next/font/local'
+
+// Font files can be colocated inside of `pages`
+const myFont = localFont({ src: "@/fonts/Nohemi-VF.ttf" })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
---
+++ routes.ts
@@ -0,0 +1,31 @@
+/*
+    * An array of public routes that do not require authentication
+    * @type {string[]}
+ */
+export const publicRoutes = [
+    "/"
+]
+/*
+    * An array of public routes that do not require authentication
+    * These routes will redirect logged users to /settings
+    * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+]
+
+/*
+    * The prefix for the API auth routes
+    * Routes that start with this prefix are used for api authentication purposes
+    * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+
+/*
+    * The default redirect route after a successful login
+    * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
