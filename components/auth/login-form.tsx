2a39486b91e5aef63a1b2ed92337695d265852d9
your name goes here <jarolimfilip07@gmail.com>
2024-02-24 20:25:01 +0100

    added providers

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {CardWrapper} from "@/components/auth/card-wrapper";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation"
 
 import * as z from "zod";
 
@@ -23,6 +24,9 @@ import { login } from '@/actions/login';
 
 export const LoginForm = () => {
 
+    const searchParams = useSearchParams()
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already in use with different provider!" : ""
+
     const [error, setError] = React.useState<string | undefined>("")
     const [success, setSuccess] = React.useState<string | undefined>("")
     const [isPending, startTransition] = React.useTransition()
@@ -42,8 +46,9 @@ export const LoginForm = () => {
 
         startTransition(() => {
             login(values).then((data) => {
-                setError(data.error)
-                setSuccess(data.success)
+                setError(data?.error)
+                // TODO: ADD WHEN WE HAVE 2FA
+                // setSuccess(data?.success)
             })
         })
     }
@@ -95,7 +100,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button type={"submit"} disabled={isPending} className={"w-full"}>
                         Login
