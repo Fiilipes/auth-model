b3cd5dedef56ad314fcb4b5555c098ad1e4b2f27
your name goes here <jarolimfilip07@gmail.com>
2024-02-27 15:34:26 +0100

    2FA

--- auth.ts
+++ auth.ts
@@ -6,6 +6,8 @@ import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
 import {UserRole} from "@prisma/client";
 
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
+
 
 export const {
     handlers: { GET, POST },
@@ -38,7 +40,14 @@ export const {
             // Prevent sign in without email verification
             if (!existingUser?.emailVerified) return false
 
-            // TODO: Add 2FA check
+            if (existingUser.isTwoFactorEnabled) {
+                const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id)
+                if (!twoFactorConfirmation) return false
+
+                await db.twoFactorConfirmation.delete({
+                    where: { id: twoFactorConfirmation.id }
+                })
+            }
 
             return true
         },
